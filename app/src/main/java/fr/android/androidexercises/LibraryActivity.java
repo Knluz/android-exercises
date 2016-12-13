f80070861c89631c83f3dc2acc1c06ca3576d1ca
Knluz <knldustbin@gmail.com>
2016-12-13 10:59:10 +0100

    deuxieme partie tps

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -1,5 +1,6 @@
 package fr.android.androidexercises;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -23,8 +24,9 @@ public class LibraryActivity extends AppCompatActivity {
         openButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Replace with startActivity() to start BookActivity
+                Intent intent = new Intent(LibraryActivity.this, BookActivity.class);
                 Toast.makeText(LibraryActivity.this, R.string.toast_todo, Toast.LENGTH_SHORT).show();
+                startActivity(intent);
             }
         });
     }
