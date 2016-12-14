2cabf9b40cb421da59771a8c4bdd6594b3e76572
Knluz <knldustbin@gmail.com>
2016-12-14 09:59:37 +0100

    retrofit use

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -3,9 +3,22 @@ package fr.android.androidexercises;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Response;
+import retrofit.Retrofit;
+import timber.log.Timber;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 public class LibraryActivity extends AppCompatActivity {
 
@@ -15,7 +28,36 @@ public class LibraryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_library);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
-        TextView messageTextView = (TextView) findViewById(R.id.messageTextView);
+        Timber.plant(new Timber.DebugTree());
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://henri-potier.xebia.fr/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        BookService service = retrofit.create(BookService.class);
+
+        Call<List<Book>> call = service.listBooks();
+        call.enqueue(new Callback<List<Book>>() {
+            @Override
+            public void onResponse(Response<List<Book>> response, Retrofit retrofit) {
+                for (Book book : response.body()) {
+                    Timber.i(book.getTitle());
+                    //Log.d(LibraryActivity.class.getCanonicalName(),book.getTitle());
+                }
+            }
+            @Override
+            public void onFailure(Throwable t) {
+                try {
+                    throw t;
+                } catch (Throwable throwable) {
+                    throwable.printStackTrace();
+                }
+                Toast.makeText(LibraryActivity.this, "Failure", LENGTH_SHORT).show();
+            }
+        });
+
+        //TextView messageTextView = (TextView) findViewById(R.id.messageTextView);
         //messageTextView.setText("Coucou !");
 
         setSupportActionBar(toolbar);
